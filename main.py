756bddada7e397a2b704326d1163a9e9bda437a0
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2020-04-14 19:28:24 +0200

    cache dataset

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 
-from utils import DatasetLoader
+from dataset_loader import DatasetLoader
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('Qt5Agg')
-
+  
 from CNN_model import CNNModel
 from no_input_model import NoInputModel
 
 def main():
     dataset_loader = DatasetLoader("data/locationsGeoguessr/",(48,64),"coordinates")
-    train_dataset = dataset_loader.load_dataset(0,9000)
-    val_dataset = dataset_loader.load_dataset(9000,10000)
+    train_dataset = dataset_loader.load_dataset(0,9000,"train_dataset")
+    val_dataset = dataset_loader.load_dataset(9000,10000,"validation_dataset")
 
     models = {
         "CNN": CNNModel(),
@@ -31,14 +31,9 @@ def main():
     plt.legend(loc='lower right')
     plt.show()
 
-    images_to_predict = []
-    labels_to_predict = []
-    for sample in train_dataset.take(9):
-        img, label = sample
-        images_to_predict.append(img)
-        labels_to_predict.append(label)
+    images_to_predict, labels_to_predict = next(iter(val_dataset))
 
-    train_predictions = models["noinput"].predict(images_to_predict)
+    test_predictions = models["CNN"].predict(images_to_predict)
     plt.figure(figsize=(10,10))
     print("yes")
     for i in range(9):
@@ -48,7 +43,7 @@ def main():
         plt.grid(False)
         print(images_to_predict[i].shape)
         plt.imshow(images_to_predict[i])
-        plt.xlabel(str(list(labels_to_predict[i].numpy()))+"\n"+str(list(train_predictions[i])))
+        plt.xlabel(str(list(labels_to_predict[i].numpy()))+"\n"+str(list(test_predictions[i])))
 
     plt.show()
 
