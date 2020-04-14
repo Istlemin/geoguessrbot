0dddc8ab68efef628674cf58585c13f221f0a85e
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2020-04-14 19:27:48 +0200

    renamed to dataset_loader

--- dataset_loader.py
+++ dataset_loader.py
@@ -15,11 +15,13 @@ class DatasetLoader():
     def get_coordinates(self, file_path):
         label_data = tf.strings.split(tf.io.read_file(file_path),sep=",")
         return tf.stack([tf.strings.to_number(label_data[0])/90,tf.strings.to_number(label_data[1])/180])
+        return tf.stack([0,0])
 
     def decode_img(self, img):
         img = tf.image.decode_jpeg(img, channels=3)
         img = tf.image.convert_image_dtype(img, tf.float32)
         return tf.image.resize(img, self.input_img_size)
+        #return tf.constant(np.zeros((48,64,3)))
 
     def read_from_index(self, file_index):
         file_index_str = tf.strings.as_string(file_index)
@@ -28,8 +30,9 @@ class DatasetLoader():
         img = self.decode_img(img)
         return img, label
 
-    def load_dataset(self,start_index, end_index, batch_size=1000):
+    def load_dataset(self,start_index, end_index, cache_file, batch_size=1000):
         indices = np.arange(start_index,end_index)
         ds_indices = tf.data.Dataset.from_tensor_slices(indices)
         datset = ds_indices.map(self.read_from_index, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        return datset.shuffle(batch_size).batch(batch_size)
+        return datset.cache(cache_file + ".cache").shuffle(batch_size).batch(batch_size)
+
